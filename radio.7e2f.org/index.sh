2ef812fd118eb59658872e66f591060848d3c889
astra <astranomaly@gmail.com>
2018-01-26 09:08:06 -0500

    radio stream link

--- radio.7e2f.org/index.sh
+++ radio.7e2f.org/index.sh
@@ -3,7 +3,8 @@
 title="~/radio"
 
 content+="\
-7e2f.org radio stream
+7e2f.org radio stream<br />
+<a href='http://7e2f.org:8000'></a>
 <audio autoplay>
 <source src='radio.7e2f.org/live.mp3' type='audio/mpeg'>
 </audio>
