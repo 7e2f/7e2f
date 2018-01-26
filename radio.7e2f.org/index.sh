b83037579469b8cb1435d49b62504fb81f14fb4e
astra <astranomaly@gmail.com>
2018-01-26 16:58:42 -0500

    update

--- radio.7e2f.org/index.sh
+++ radio.7e2f.org/index.sh
@@ -4,7 +4,7 @@ title="radio"
 
 content+="\
 7e2f.org radio stream<br />
-<audio loop autoplay>
+<audio loop autoplay controls>
 <source src='http://7e2f.org:8000/mpd.ogg' type='audio/ogg'>
 </audio>
 "
